39714e83a92cb9059b1e7ea859a54cda8f0f2dbd
TehAppKiller <47398236+TehAppKiller@users.noreply.github.com>
2026-05-04 05:40:50 +0200

    lockfile removed on startup
    # Remove the lockfile if the process is not terminated correctly (bug on snapcraft refresh to 5.2.0) # TODO: Find a proper way ?

--- snap/local/bin/run-qbittorrent.sh
+++ snap/local/bin/run-qbittorrent.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -e
 
+# Remove the lockfile if the process is not terminated correctly (bug on snapcraft refresh to 5.2.0) # TODO: Find a proper way ?
+rm $SNAP_USER_DATA/.config/qBittorrent/lockfile
+
+# Launch app
 "$SNAP/usr/bin/qbittorrent-nox"
